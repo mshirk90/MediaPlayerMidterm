9c7aa6e7dd47f5f1d64f7c0bce509e810ecfb9f6
mshirk90 <mshirk90@gmail.com>
2016-09-27 20:13:10 -0500

    first commit

---
+++ MediaPlayerMidterm/MediaPlayerMidterm/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Text;
+using System.IO;
+
+namespace MediaPlayerMidterm
+{
+    public partial class _Default : Page
+    {
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+        }
+     
+
+    }
+}
---
+++ MediaPlayerMidterm/MediaPlayerMidterm/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MediaPlayerMidterm.Global" Language="C#" %>
